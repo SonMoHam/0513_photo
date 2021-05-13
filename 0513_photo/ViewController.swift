32871582e53cf5d2a2a67cf1d22e13b6f4996576
SonMoHam <dnddl505@gmail.com>
2021-05-13 14:41:32 +0900

    Initial Commit

---
+++ 0513_photo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  0513_photo
+//
+//  Created by 손대홍 on 2021/05/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
